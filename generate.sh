dc3a2ba38d5dc91acddfe43a21393a909665392e
Kiyoshi '13k' Murata <kbmurata@gmail.com>
2017-05-28 06:43:51 -0300

    Add a Small reorganization to generate.sh

--- generate.sh
+++ generate.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e
+
 SCRIPT_PATH="$(realpath "$0")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 
-go run "gen.go" "$SCRIPT_DIR" && \
-	go build ./protobuf
+go run "gen.go" "$SCRIPT_DIR"
+go build ./protobuf
