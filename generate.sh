690d4d048825c38541607b4937dd4e6d8ec2f497
Kiyoshi '13k' Murata <kbmurata@gmail.com>
2017-06-09 20:33:10 -0300

    Add some information messages to generate.sh :nail_care:

--- generate.sh
+++ generate.sh
@@ -5,7 +5,19 @@ set -e
 SCRIPT_PATH="$(realpath "$0")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 
+green="\e[032m"
+reset="\e[0m"
+
+printf "${green}Cleaning protobuf packages ...${reset}\n"
 rm -rf "./protobuf"
+
+printf "${green} * ${reset}Generating stubs ...\n"
 go run "gen.go" "$SCRIPT_DIR"
+
+printf "${green} * ${reset}Generating protobuf packages ...\n"
 go generate "./protobuf/..."
+
+printf "${green} * ${reset}Building protobuf packages ...\n"
 go build "./protobuf/..."
+
+printf "${green} * ${reset}Done!\n"
